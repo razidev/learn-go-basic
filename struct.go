72da2f45b7d6c646f1c8750c0ab50a55a8e58581
razidev <razisyahputro@gmail.com>
2021-12-30 15:21:50 +0700

    learn struct embed

--- struct.go
+++ struct.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type job struct {
+	Position string
+	Salary   int
+}
+
 type MySelf struct {
 	Name, Address string
 	Age           int
+	job
 }
 
 //struct function
@@ -17,17 +23,28 @@ func main() {
 	Me.Name = "Razi"
 	Me.Address = "Kp. Mangga"
 	Me.Age = 25
-	fmt.Println(Me)
+	Me.job.Salary = 1000
+	Me.Position = "Fullstack Developer"
+	fmt.Println("Me:", Me)
 	Me.sayHi("Joko")
 
 	aziz := MySelf{
 		Name:    "Aziz",
 		Address: "Gg. Teladan 5",
-		Age:     24,
+		job: job{
+			Salary:   3000,
+			Position: "Frontend Developer",
+		},
 	}
-	fmt.Println(aziz)
+	fmt.Println("aziz:", aziz)
+	//tidak rekomendasi tidak menggunakan .job karna bisa redundan jika ada struct Position
+	fmt.Println("aziz position:", aziz.Position)
+
+	fmt.Println("aziz salary:", aziz.job.Salary)
 
 	//tidak disarankan krn ketergantungan posisi
-	syahputro := MySelf{"Syahputro", "Jakarta", 23}
-	fmt.Println(syahputro)
+	syahputro := MySelf{"Syahputro", "Jakarta", 23, job{"Cloud Developer", 20000}}
+	fmt.Println("syahputro:", syahputro)
+	fmt.Println("syahputro salary:", syahputro.Salary)
+	fmt.Println("syahputro position:", syahputro.job.Position)
 }
