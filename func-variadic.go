9c0f3dc92ab22c8945dedb1d029c45dbef33c90a
razidev <razisyahputro@gmail.com>
2022-01-04 16:08:44 +0700

    add parameter

--- func-variadic.go
+++ func-variadic.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func sumAll(numbers ...int) int { //varargs must be in the end of parameters / at the right not middle or left
+func sumAll(title string, numbers ...int) int { //varargs must be in the end of parameters / at the right not middle or left
 	total := 0
-	fmt.Println("value", numbers)
+	fmt.Println("value", numbers, title)
 	for _, val := range numbers {
 		total += val
 	}
@@ -12,10 +12,10 @@ func sumAll(numbers ...int) int { //varargs must be in the end of parameters / a
 }
 
 func main() {
-	total := sumAll(10, 9, 5, 3)
+	total := sumAll("First", 10, 9, 5, 3)
 	fmt.Println("total", total)
 
 	slice := []int{10, 5, 0, 20}
-	total = sumAll(slice...)
+	total = sumAll("Second", slice...)
 	fmt.Println("total", total)
 }
