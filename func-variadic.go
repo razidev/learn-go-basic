74aa2b2a8428037d57c3691ae1ed6bb80108e83b
razidev <razisyahputro@gmail.com>
2022-01-04 16:05:23 +0700

    learn variadic function

---
+++ func-variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sumAll(numbers ...int) int { //varargs must be in the end of parameters / at the right not middle or left
+	total := 0
+	fmt.Println("value", numbers)
+	for _, val := range numbers {
+		total += val
+	}
+	return total
+}
+
+func main() {
+	total := sumAll(10, 9, 5, 3)
+	fmt.Println("total", total)
+
+	slice := []int{10, 5, 0, 20}
+	total = sumAll(slice...)
+	fmt.Println("total", total)
+}
