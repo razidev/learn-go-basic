738cd3b4c3e66a15f8a9ef9caae8ef01e84e4631
razidev <razisyahputro@gmail.com>
2022-01-07 16:21:05 +0700

    add more example

--- func-anonym.go
+++ func-anonym.go
@@ -15,7 +15,14 @@ func main() {
 	blacklist := func(name string) bool {
 		return name == "admin"
 	}
-
 	registerUser("admin", blacklist)
 	registerUser("Razi", blacklist)
+
+	func() {
+		fmt.Println("I am anonymous func too")
+	}()
+
+	func(x int) {
+		fmt.Println("I am anonymous func too with a number of", x)
+	}(22)
 }
