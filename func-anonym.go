3e5ab404a7995820eb73b07331e06cb939efdcc3
razidev <razisyahputro@gmail.com>
2022-01-13 23:57:54 +0700

    add more example

--- func-anonym.go
+++ func-anonym.go
@@ -4,6 +4,8 @@ import "fmt"
 
 type Blacklist func(string) bool
 
+var f2 func()
+
 func registerUser(name string, blacklist Blacklist) {
 	if blacklist(name) {
 		fmt.Println("You are blocked", name)
@@ -25,4 +27,15 @@ func main() {
 	func(x int) {
 		fmt.Println("I am anonymous func too with a number of", x)
 	}(22)
+
+	f1 := func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println(i)
+		}
+	}
+	f1()
+
+	f2 = f1
+	fmt.Printf("%T F2\n", f2)
+	f2()
 }
