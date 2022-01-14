b1541fddbf75c0f5af4ddac6bfc5e92ad3d81cb2
razidev <razisyahputro@gmail.com>
2022-01-14 19:59:51 +0700

    learn pointer

---
+++ pointer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	var address1 Address = Address{"Subang", "Jawa Barat", "Indonesia"}
+	var address2 Address = address1
+	var address3 *Address = &address2
+
+	address3.City = "Bandung"
+	// address3 = &Address{"Jakarta", "DKI Jakarta", "Indonesia"} //create new memory for address3
+	*address3 = Address{"Jakarta", "DKI Jakarta", "Indonesia"} //create new same memory for address3 & address2
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+	fmt.Println(address3)
+
+	//create a empty pointer
+	var address4 *Address = new(Address)
+	address4.Province = "DIY Jogjajakarta"
+	fmt.Println(address4)
+}
