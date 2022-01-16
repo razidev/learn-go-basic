7b38ae27f93bf97c84d5647c74a10ef14b212369
razidev <razisyahputro@gmail.com>
2022-01-16 17:56:25 +0700

    learn access modifier

--- helper/sayHello.go
+++ helper/sayHello.go
@@ -2,6 +2,15 @@ package helper
 
 import "fmt"
 
+var version = "1.8.9"
+var Application = "Belajar Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
 }
+
+func sayGoodBye(name string) {
+	fmt.Println("Good bye", name)
+}
+
+//if variable or function started with Uppercase it means public, vice versa
--- import.go
+++ import.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"Udemy-PZN/helper"
+	"fmt"
 )
 
 func main() {
 	helper.SayHello("Razi")
+	// helper.sayGoodBye("Razi") //not public
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) //not public
 }
