4e340090f709eaaadad6b3b9198cf45bd7dbf584
razidev <razisyahputro@gmail.com>
2022-01-21 16:59:11 +0700

    add more data example

--- json-marshal.go
+++ json-marshal.go
@@ -6,9 +6,17 @@ import (
 )
 
 type Person struct {
-	Name    string         `json:"name"`
-	Hobbies []string       `json:"hobbies"`
-	Books   map[int]string `json:"books"`
+	Name      string         `json:"name"`
+	Hobbies   []string       `json:"hobbies"`
+	Books     map[int]string `json:"books"`
+	Addresses []Address      `json:"address"`
+}
+
+type Address struct {
+	Name       string `json:"name"`
+	Street     string `json:"street"`
+	Country    string `json:"country"`
+	PostalCode string `json:"postalcode"`
 }
 
 func main() {
@@ -19,6 +27,20 @@ func main() {
 			1: "Atomic Habits",
 			2: "The Intelligent Investor",
 			3: "Supernova",
+		},
+		Addresses: []Address{
+			{
+				Name:       "Home",
+				Street:     "Jl. Nusantara",
+				Country:    "Indonesia",
+				PostalCode: "14444",
+			},
+			{
+				Name:       "Office",
+				Street:     "Jl. Megapolitan",
+				Country:    "Indonesia",
+				PostalCode: "14440",
+			},
 		},
 	}
 	p2 := Person{
