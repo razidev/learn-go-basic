1bde8f02e41f8540dfef422f4322bc7bb7df1890
razidev <razisyahputro@gmail.com>
2022-01-24 22:03:23 +0700

    learn atomic

---
+++ atomic.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var x int64 = 0
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			group.Add(1)
+			for j := 1; j <= 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	fmt.Println("counter = ", x)
+}
