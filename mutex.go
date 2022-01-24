69896a17b081e7170e9fafdee5245f3a67af9b13
razidev <razisyahputro@gmail.com>
2022-01-24 21:50:34 +0700

    add waitgroup

--- mutex.go
+++ mutex.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
-	"time"
 )
 
 func main() {
 	x := 0
 	var mutex sync.Mutex
+	var wg sync.WaitGroup
+	fmt.Println("CPU:", runtime.NumCPU())
+	fmt.Println("Goroutines", runtime.NumGoroutine())
+
+	const num = 100
+	wg.Add(100000)
 
 	for i := 1; i <= 1000; i++ {
 		go func() {
-			for j := 1; j <= 100; j++ {
+			for j := 1; j <= num; j++ {
 				mutex.Lock()
+				runtime.Gosched()
 				x++
 				mutex.Unlock()
+				wg.Done()
 			}
 		}()
 	}
-
-	time.Sleep(5 * time.Second)
+	wg.Wait()
+	fmt.Println("CPU:", runtime.NumCPU())
+	fmt.Println("Goroutines", runtime.NumGoroutine())
 	fmt.Println("counter = ", x)
 }
