c7b1aaf9e0c79c4bd62b2c66677da96cb972c628
razidev <razisyahputro@gmail.com>
2022-01-30 19:06:03 +0700

    learn context value

---
+++ context-value.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b")) //different parent
+	fmt.Println(contextA.Value("b")) //parent cannot take the value's child
+}
