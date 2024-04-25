ea99071bfb812da979c273159c87a1be4caf15fb
razisyahputro <razisyahputro@gmail.com>
2024-04-25 17:27:42 +0700

    learn basic string go

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Razi")
+	fmt.Println(len("Razi"))
+
+	//hasil berupa byte harus dikonversi dahulu
+	fmt.Println("Razi"[0])
+	fmt.Println("Razi"[2])
+}
