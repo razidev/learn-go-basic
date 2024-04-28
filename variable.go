89c06e13ba5ebce0f9dd39e1a5c762e6b76d0c28
razisyahputro <razisyahputro@gmail.com>
2024-04-28 21:50:00 +0700

    learn variable

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// VARIOUS KINDS OF STRINGS
+	// var name string
+	// var name = "Razi"
+	// name := "Razi"
+	// name = "Razi"
+
+	var (
+		name = "Razi"
+		age  = 20
+		city = "Jakarta"
+	)
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(city)
+	name = "aziz"
+	fmt.Println(name)
+}
