b2bab39ebff6e40c5438b8000e1b807fa92380f3
razisyahputro <razisyahputro@gmail.com>
2024-04-28 21:51:44 +0700

    add sample

--- type-declaration.go
+++ type-declaration.go
@@ -8,9 +8,12 @@ func main() {
 
 	var myKtp NoKTP = "123455677854"
 	var marriage Married = false
+	var contoh string = "222222"
+	var contohKTP NoKTP = NoKTP(contoh)
 
 	fmt.Println(myKtp)
 	fmt.Println(marriage)
+	fmt.Println(contohKTP)
 }
 
 // type declaration uses for initialize with the name you want
