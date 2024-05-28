70dee1afcc15adf70f8a084de049b1ff81a3e5d8
razisyahputro <razisyahputro@gmail.com>
2024-05-28 23:32:31 +0700

    change exmpale error custom

--- error-custom.go
+++ error-custom.go
@@ -1,26 +1,56 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type customErr struct {
-	info string
+type validationError struct {
+	Message string
 }
 
-func (ce customErr) Error() string {
-	return fmt.Sprintf("here is the error: %v", ce.info)
+func (v *validationError) Error() string {
+	return v.Message
+}
+
+type notFoundError struct {
+	Message string
+}
+
+func (n *notFoundError) Error() string {
+	return n.Message
+}
+
+func SaveData(id string, data any) error {
+	if id == "" {
+		return &validationError{"validation error"}
+	}
+
+	if id != "eko" {
+		return &notFoundError{"data not found"}
+	}
+
+	return nil
 }
 
 func main() {
-	c1 := customErr{
-		info: "this is the custom error",
+	err := SaveData("eko", nil)
+	if err != nil {
+		// terjadi error
+		//if validationErr, ok := err.(*validationError); ok {
+		//	fmt.Println("validation error:", validationErr.Error())
+		//} else if notFoundErr, ok := err.(*notFoundError); ok {
+		//	fmt.Println("not found error:", notFoundErr.Error())
+		//} else {
+		//	fmt.Println("unknown error:", err.Error())
+		//}
+
+		switch finalError := err.(type) {
+		case *validationError:
+			fmt.Println("validation error:", finalError.Error())
+		case *notFoundError:
+			fmt.Println("not found error:", finalError.Error())
+		default:
+			fmt.Println("unknown error:", finalError.Error())
+		}
+	} else {
+		fmt.Println("Sukses")
 	}
-
-	foo(c1)
-}
-
-func foo(e error) {
-	fmt.Println(e)
-	fmt.Println(e.(customErr).info)
 }
